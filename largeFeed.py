efd08719231779bb43e2efa0ab08ec835e502883
mchandx <matt@cubalo.com>
2015-07-31 20:33:57 -0400

    debugging

--- largeFeed.py
+++ largeFeed.py
@@ -11,7 +11,7 @@ import sys
 # Tell the system how to drive the stepper
 sequence = [[1.0, 1.0], [1.0, -1.0], [-1.0, -1.0], [-1.0, 1.0]]  # Order for stepping
 stepDelay = 0.002                                                # Delay between steps
-degPerStep = 1.8                                                 # Number of degrees moved per step
+degPerStep = 0.18                                                 # Number of degrees moved per step
 stepperVoltage = 3.0
 batteryVoltage = 6.0
 pwmLevel = stepperVoltage / batteryVoltage
@@ -91,7 +91,7 @@ try:
     # Start by turning all drives off
     PBR.MotorsOff()
     # Rotate 3 full revolutions
-    MoveDeg(360 * 9)
+    MoveDeg(360 * 3)
     PBR.MotorsOff()
     # Log message to a file.
     logging.info("Large amount dispensed on " + feedTime)
